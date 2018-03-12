035fc150f5c5df077342b7e0088fa21772bbc322
David Levine <levine.1726@osu.edu>
2018-03-12 13:36:56 -0400

    Created and outlined input.js file.
    input.js now contains an definition for an input object, and also function definitions for all other functions to be implemented. Since this is an outline, no methods have been implemented yet.

---
+++ source/javascripts/input.js
@@ -0,0 +1,91 @@
+/*
+  CREATED: David Levine 03/12/2018
+  
+  The following file is responsible for dealing with all input 
+  to the calculator through the gui.
+*/
+
+/*
+  CREATED: David Levine 03/12/2018
+
+  Description: Add element to string based off cursor location
+
+  parameters: 
+    element: element to add to string
+
+  returns:
+    undefined
+*/
+function addToString(element) {
+
+}
+
+
+/*
+  CREATED: David Levine 03/12/2018
+
+  Description: Returns input string to user
+
+  parameters: 
+    element: element to add to string
+
+  returns:
+    undefined
+*/
+function returnInputString() {
+
+}
+
+/*
+  CREATED: David Levine 03/12/2018
+  
+  Description: shifts the cursor one position to the left
+*/
+function shiftCursorLeft() {
+
+}
+
+/*
+  CREATED: David Levine 03/12/2018
+  
+  Description: shifts the cursor one position to the left
+*/
+function shiftCursorRight() {
+
+}
+
+/*
+  CREATED: David Levine 03/12/2018
+  
+  Description: writes the updated input to the DOM 
+
+  parameters:
+    htmlEl: the html element where you want to write input to
+
+  updates:
+    the inner html specified at htmlEl
+*/
+function writeInputToDOM(htmlEl) {
+
+}
+
+
+/* 
+  CREATED: David Levine 03/12/2018
+
+  Description: Used to return an object that lets a user interact with the 
+  html box on the web gui. 
+*/ 
+function input(htmlEl) {
+  /* initalize properties */
+  this.leftString = "";
+  this.rightString = "";
+  this.cursor = "<span>|</span>";
+  this.htmlEl = htmlEl;
+
+  /* Functions to manipulate input string */
+  this.addToString = addToString;
+  this.returnInputString = returnInputString;
+  this.shiftCursorLeft = shiftCursorLeft; 
+  this.shiftCursorRight = shiftCursorRight;   
+}
