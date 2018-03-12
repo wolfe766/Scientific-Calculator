8c673bc3412d3a950b861796671065136fe55da4
David Levine <levine.1726@osu.edu>
2018-03-12 16:48:03 -0400

    Implemented calculateFactor and calculateTerm.
    Updated rdp.js with implementations for calculateTerm and calculateFactor. Also created supporting functions titled "evaluateFunction" to break down the implementation a bit more. Also wrote a factorial function, as Javascript does not have one built in.

--- source/javascripts/rdp.js
+++ source/javascripts/rdp.js
@@ -24,7 +24,11 @@
   Description: implements the parse method for the non-terminal 
   symbol term. A term consists of zero or more mulitplication operations against 
   another term. Using a CFG, the equivalent derivation is: 
-    - factor -> {mult-op factor}
+    - term -> factor {mult-op factor}
+
+  Implementation heavily guided by OSU CSE 2231 Recursive Descent Parsing slides,
+  which can be found here: 
+      -http://web.cse.ohio-state.edu/software/2231/web-sw2/extras/slides/27.Recursive-Descent-Parsing.pdf
 
   parameters: 
     -tokenQueue: a queue of tokens containing a term we want to parse
@@ -32,7 +36,17 @@
   return: the value of the term
 */
 function calculateTerm(tokenQueue) {
-
+  var value = calculateFactor(tokenQueue);
+  /* begin looping for multiplication operations */
+  while (tokenQueue[0] == "*" || tokenQueue[0] == "/") {
+    var operation = tokenQueue.shift();
+    if (operation == "*") {
+      value *= calculateFactor(tokenQueue);
+    } else {
+      value /= calculateFactor(tokenQueue);
+    }
+  }
+  return value;
 }
 
 /*
@@ -48,5 +62,101 @@ function calculateTerm(tokenQueue) {
   return: the value of the factor.
 */
 function calculateFactor(tokenQueue) {
+  /* Establish valid unary function names to check for */
+  funcNames = ["fact", "sin", "cos", "tan", "sqrt"];
+  var value;
+  var token = tokenQueue.shift();    
+  /* expression wraped in parenthesis */
+  if (token == "(") {
+    value = calculateExpression(tokenQueue);
+    /* remove trailing parenthesis */
+    tokenQueue.shift();
+  /* expression wrapped in a uniary function */
+  } else if (funcNames.includes(token)) {
+    var funcString = funcNames[funcNames.indexOf(token)];
+    value = evaluateFunction(funcString, parseFloat(tokenQueue.shift));
+    /* remove trailing parenthesis */
+    tokenQueue.shift();
+  /* Two expressions wrapped in a power function*/
+  } else if (token == "pow") {
+    /* discard "pow" and "(" tokens */
+    tokenQueue.shift();
+    tokenQueue.shift();
+    /* evaluate first and second expression in pow */
+    var expr1val = calculateExpression(tokenQueue);
+    tokenQueue.shift();
+    var expr2val = calculateExpression(tokenQueue);
+    /* calculate the power value */
+    value = pow(expr1val, expr2val);
+    /* remove trailing parenthesis */
+    tokenQueue.shift();
+  /* otherwise, we are only dealing with a number */
+  } else {
+    value = parseFloat(token);
+  }
+  return value;
+}
 
+
+/*
+  CREATED: David Levine 03/12/2018
+
+  Description: calculates the value of a unary function given 
+  a string representing the function and the corresponding value
+  to evaluate.
+
+  parameters: 
+    -funcString: a string representing one of five supported functions, 
+     "fact" for factorial, "sin" for sine, "cos" for cosine, "tan" for tangent,
+     and "sqrt" for square root.
+    -funcParam: the parameter to pass into the function.
+
+  return: the value of the factor.
+*/
+function evaluateFunction(funcString, funcParam) {
+  var valToReturn;
+  switch (funcString) {
+    case "fact":
+      valToReturn = factorial(funcParam);
+      break;
+
+    case "sin":
+      valToReturn = sin(funcParam * Math.PI / 180);
+      break;
+
+    case "cos":
+      valToReturn = cos(funcParam * Math.PI / 180);
+      break;
+
+    case "tan":
+      valToReturn = tan(funcParam * Math.PI / 180);
+      break;
+
+    case "sqrt":
+      valToReturn = sqrt(funcParam);
+      break;
+  }
+
+  return valToReturn;
+}
+
+/*
+  CREATED: David Levine 03/12/2018
+
+  Description: recursively calculates the factorial of a
+  given number.
+
+  parameters: 
+    -value: the number to find the factorial of
+
+  requires: value is greater than 0
+
+  return: the factorial value.
+*/
+function factorial(value) {
+  returnVal = 0;
+  if (value != 0) {
+    returnVal = value * factorial(value - 1);
+  }
+  return returnVal;
 }
