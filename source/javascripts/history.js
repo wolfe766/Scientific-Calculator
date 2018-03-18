dda4f91633f5f98a5a1d73f078e9e8bfeb1b6208
Sam Wolfe <wolfe.766@osu.edu>
2018-03-18 04:41:34 -0400

    Added smaller history bar and support for infinite entries

--- source/javascripts/history.js
+++ source/javascripts/history.js
@@ -3,48 +3,106 @@ Description: This file manages both the smaller history
 box and the big history box. 
 */
 
-//The stack of all past entries.
-var entryStack = [];
+//Number of entries to display in the history at one time
+//Edit to account for size of history display
+const BIG_HISTORY_ENTRY_SIZE = 7;
+const SMALL_HISTORY_ENTRY_SIZE = 2;
 
 /*CREATED: Sam Wolfe 3/18/2018
 Description: Constructor method for the Entry object.
 
 Parameters:
-	eq: String - The equation
-	ans: String - The Answer (or Error)
+  eq: String - The equation
+  ans: String - The Answer (or Error)
 */
 function Entry(eq, ans){
-	this.equation = eq;
-	this.answer = ans;
-	//We could automatically add this to the entryStack right here if desired
+  this.equation = eq;
+  this.answer = ans;
+  //We could automatically add this to the entryStack right here if desired
 }
 
 /*CREATED: Sam Wolfe 3/18/2018
-Description: Prints the given entry to the approriate history container
-and updates history containers accordingly.
-
-Parameters:
-	entry: Entry - The entry to be written to history
+Description: Controller object for everything related to the history boxes
 */
-function printToHistory(entry){
+var historyController = 
+{
+  //The stack of all past entries.
+  entryStack: [],
 
-	var container = document.getElementById("historyContainer");
+  //Num entries currently in each history box
+  largeHistoryEntryCount: 0,
+  smallHistoryEntryCount: 0,
 
-	var historyElement = document.createElement("div");
-	historyElement.className = "historyElement";
+  /*CREATED: Sam Wolfe 3/18/2018
+  Description: Generates an HTML element to place inside of the
+  history box.
 
-	var eq = document.createElement("p");
-	eq.className = "historyEq";
-	var eqText = document.createTextNode(entry.equation);
-	eq.appendChild(eqText);
+  Parameters:
+      entry: Entry - Object containing information to be contained in element
+  */
+  generateHistoryElement: function(entry){
+    var historyElement = document.createElement("div");
+    historyElement.className = "historyElement";
 
-	var ans = document.createElement("p");
-	ans.className = "historyAns";
-	var ansText = document.createTextNode(entry.answer);
-	ans.appendChild(ansText);
+    var eq = document.createElement("p");
+    eq.className = "historyEq";
+    var eqText = document.createTextNode(entry.equation);
+    eq.appendChild(eqText);
 
-	historyElement.appendChild(eq);
-	historyElement.appendChild(ans);
+    var ans = document.createElement("p");
+    ans.className = "historyAns";
+    var ansText = document.createTextNode(entry.answer);
+    ans.appendChild(ansText);
 
-	container.insertBefore(historyElement,container.firstChild);
+    //Add generated elements to appropriate containers
+    historyElement.appendChild(eq);
+    historyElement.appendChild(ans);
+
+    return historyElement;
+  },
+
+  /*CREATED: Sam Wolfe 3/18/2018
+  Description: Prints the given entry to the approriate history container
+  and updates history containers accordingly.
+
+  Parameters:
+   entry: Entry - The entry to be written to history
+  */
+  addToHistory: function(entry){
+    //Add this entry to the stack
+    this.entryStack.push(entry);
+
+    //Create HTML elements for approriate history containers
+    var bigContainer = document.getElementById("bigHistory");
+    var smallContainer = document.getElementById("smallHistory");
+
+    historyElementBig = this.generateHistoryElement(entry);
+    historyElementSmall = historyElementBig.cloneNode(true);
+
+    bigContainer.insertBefore(historyElementBig,bigContainer.firstChild);
+    smallContainer.insertBefore(historyElementSmall,smallContainer.firstChild);
+
+    //Increment history count, remove overflow
+    this.largeHistoryEntryCount++;
+    this.smallHistoryEntryCount++;
+    historyController.removeHistoryOverflow();
+  },
+
+  /*CREATED: Sam Wolfe 3/18/2018
+  Description: Adjusts the view of the HTML object when the history
+  boxes become full
+  */
+  removeHistoryOverflow: function (){
+    if (this.largeHistoryEntryCount >= BIG_HISTORY_ENTRY_SIZE){
+      bigContainer = document.getElementById("bigHistory");
+      bigContainer.removeChild(bigContainer.lastChild);
+      this.largeHistoryEntryCount--;
+    }
+
+    if (this.smallHistoryEntryCount >= SMALL_HISTORY_ENTRY_SIZE){
+      smallContainer = document.getElementById("smallHistory");
+      smallContainer.removeChild(smallContainer.lastChild);
+      this.smallHistoryEntryCount--;
+    }
+  }
 };
--- test/testJS/testHistory.js
+++ test/testJS/testHistory.js
@@ -1,13 +1,19 @@
 /*
 	CREATED: Sam Wolfe 3/18/2018
 	Description: Tests for the history objects.
-
+	
+	Equations and answers generated in this final will NOT BE CORRECT
+	they're strictly used for formatting
 */
-var testEntry = new Entry("12+5","17");
+var equation = Math.floor(Math.random() * 100).toString() + " + " + Math.floor(Math.random() * 100).toString();
+var answer = Math.floor(Math.random() * 100).toString();
+var testEntry = new Entry(equation, answer);
 
 input = document.getElementById("input");
 input.addEventListener("click", function(){
-	printToHistory(testEntry);
+	historyController.addToHistory(testEntry);
+	testEntry.equation = Math.floor(Math.random() * 100).toString() + " + " + Math.floor(Math.random() * 100).toString();
+	testEntry.answer = Math.floor(Math.random() * 100).toString();
 });
 
 //Will be more fully fleshed out in the future once actual
